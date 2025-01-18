537ae8015514f7f0281fb51a8524c30c41f5be73
RinZhiming <me.jaratpong@gmail.com>
2025-01-18 19:59:48 +0700

    add chat folder and init chat widget

---
+++ lib/chat/message.dart
@@ -0,0 +1,31 @@
+import 'package:ai_chat_frontend/chat/models/message_response.dart';
+import 'package:flutter/material.dart';
+
+class Message extends StatelessWidget {
+  final EdgeInsetsGeometry? margin;
+  final MessageResponse message;
+
+  const Message({super.key, this.margin, required this.message});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: mainAxisAlignment,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+          margin: margin ?? EdgeInsets.zero,
+          child: Text(
+            message.text,
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  MainAxisAlignment get mainAxisAlignment =>
+      message.isBot ? MainAxisAlignment.start : MainAxisAlignment.end;
+}
---
+++ lib/chat/models/message_response.dart
@@ -0,0 +1,9 @@
+class MessageResponse {
+  final String text;
+  final bool isBot;
+  
+  const MessageResponse({
+    required this.text,
+    required this.isBot,
+  });
+}
